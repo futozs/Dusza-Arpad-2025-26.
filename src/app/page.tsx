6d69ad605377c17acfed4cd5c907025837a8d1ca
Zsombi <zsombi.futo@gmail.com>
2025-11-06 21:20:26 +0100

    live share test
    Co-authored-by: szucsng <szucsng@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-white">
-      <h1 className="text-4xl font-semibold">hello dusza</h1>
+      <h1 className="text-4xl font-semibold">hello dusza!</h1>
+      
     </div>
   );
 }
