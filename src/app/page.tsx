ecfc903f215966a66e3b4b235102305ffc1341d2
futozs <zsombi.futo@gmail.com>
2025-11-06 19:15:53 +0100

    project init

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white">
+      <h1 className="text-4xl font-semibold">hello dusza</h1>
+    </div>
+  );
+}
